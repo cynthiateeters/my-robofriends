8029c906448b0cd831a16ce9c0782b2ccbc12a2d
Cynthia Teeters <glean@cynthiateeters.com>
2019-06-24 13:28:27 -0400

    make Card app and populate

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+  {
+    id: 1,
+    name: "FUELWORKS",
+    username: "Ratliff",
+    email: "Ratliff@tanzi.us"
+  },
+  {
+    id: 2,
+    name: "BLUE EYERIS",
+    username: "Cabrera",
+    email: "Cabrera@alfra.me"
+  },
+  {
+    id: 3,
+    name: "MEG ALLAY",
+    username: "Patterson",
+    email: "Patterson@poppo.com"
+  },
+  {
+    id: 4,
+    name: "TWIN FIST",
+    username: "Dillard",
+    email: "Dillard@miniman.org"
+  },
+  {
+    id: 5,
+    name: "ROOF LORIA",
+    username: "Callahan",
+    email: "Callahan@toronto.ca"
+  },
+  {
+    id: 6,
+    name: "ISOP KIPLEX",
+    username: "Rose",
+    email: "Rose@londonmail.co.uk"
+  }
+];
