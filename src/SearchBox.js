b3a07129fd6320ec0afc529ed9f23c01ef98ab3c
Cynthia Teeters <glean@cynthiateeters.com>
2019-06-25 17:15:00 -0400

    style changes

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -26,7 +26,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
  return (
   <div className='pa2'>
    <input
-    className='pa3 ba b--green bg-lightest-blue'
+    className='pa3 ba b--green bg-lightest-blue text-input'
     type='search'
     placeholder='search robot name'
     onChange={searchChange}
