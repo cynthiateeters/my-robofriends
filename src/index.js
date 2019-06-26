df8c4aa94a8162d4938bf4a5dded5409a0b709ce
Cynthia Teeters <glean@cynthiateeters.com>
2019-06-26 14:48:00 -0400

    add typescript; fix connect not used

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import { searchRobots } from './reducers';
 import App from './containers/App';
 import './index.css';
