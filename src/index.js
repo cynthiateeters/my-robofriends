a47d610eb1877a8163f3d3982a8910e270473064
Cynthia Teeters <glean@cynthiateeters.com>
2019-06-26 13:50:56 -0400

    add Redux but no connector

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { searchRobots } from './reducers';
 import App from './containers/App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
+const store = createStore(searchRobots);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+ <Provider store={store}>
+  <App  />
+ </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
